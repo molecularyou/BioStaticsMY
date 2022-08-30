6051248dfee9262cd48cd77357888f676bbf28a0
pranavtandonMY <111784856+pranavtandonMY@users.noreply.github.com>
2022-08-29 20:23:27 -0700

    update: required columns and sub categories added

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import pandas as pd
 df_in = pd.read_csv(
     'filepath.csv')
 
-d = DFmaker(df_in) #input df for reference rangecalculation
-df_exp = d.df_out()
-df_exp.to_csv (r'sample_ref.csv', index = False, header=True) for CSV maker
+#always make sure that the biomarkers are up to date in the MYBiomarkers.csv file
 
+# DFmaker{dataframe, age category "Adult"/"Child" , sex "Male"/"Female", BIOFLUD type "plasma"/"serum"}
+d = DFmaker(df_in,None,None, None) #input df for reference range calculation
 
+df_exp = d.df_out().T
+df_exp.columns = ['measure_id', 'range', 'age', 'sex','biofluid','category', 'exported' ] #add more columns for future
+df_exp.to_csv (r'sample_ref.csv', index = False, header=True) # for CSV maker
