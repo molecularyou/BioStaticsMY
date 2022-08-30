321c2c5a70b0a7a34bbe407fff5adb053b528edf
pranavtandonMY <111784856+pranavtandonMY@users.noreply.github.com>
2022-08-29 23:53:21 -0700

    Documentation Updates

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 
 df_in = pd.read_csv(
-    'filepath.csv')
+    '/Users/pranavdhruv/Desktop/MY2022/MedCalcProject/SampleData/reference_range/medcalc_input_serum_metabolomics.csv')
+
 
 #always make sure that the biomarkers are up to date in the MYBiomarkers.csv file
 
-# DFmaker{dataframe, age category "Adult"/"Child" , sex "Male"/"Female", BIOFLUD type "plasma"/"serum"}
 d = DFmaker(df_in,None,None, None) #input df for reference range calculation
 
 df_exp = d.df_out().T
-df_exp.columns = ['measure_id', 'range', 'age', 'sex','biofluid','category', 'exported' ] #add more columns for future
+df_exp.columns = ['measure_id', 'range', 'age', 'sex','biofluid','category', 'exported' ] #add more columns for future and update 
+# in the dictionary inside DFmaker
 df_exp.to_csv (r'sample_ref.csv', index = False, header=True) # for CSV maker
